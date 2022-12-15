6f40a3e6a2426b9e20ddb344914ad823b4581d1b
Giacomo Gagliano <giacomo.gagliano@me.com>
2022-12-15 17:21:35 +0100

    TODO

--- packages/ui/src/HTML/React/classes/_app/NoizApp/NoizApp_v2.tsx
+++ packages/ui/src/HTML/React/classes/_app/NoizApp/NoizApp_v2.tsx
@@ -405,6 +405,7 @@ export class NoizApp_v2 extends BaseNoiz<
     return prev !== curr;
   }
 
+  // TODO aggiungere questa in BaseNoiz
   UpdateHandler = class<T> {
     current?: T;
     previous?: T;
