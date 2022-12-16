4989e3ad90aab9be2d2ca892c332782c813a43f0
ariannatnl <arianna.minucci@me.com>
2022-12-16 08:11:23 +0100

    correzzioni mattinA

--- packages/zionbase/test/zionbase/Types/Class/GClassType.test.ts
+++
@@ -1,9 +0,0 @@
-// rappresenta il paccheto con le funzioni per i test
-import { testEnvironment } from "@zionstate/test";
-// rappresenta il codice che stiamo testando.
-// deve essere preso dall'endpoint che stiamo valutando
-import {} from "../../../../../zionbase/src/zionbase/Types/Class";
-
-const { expect, log } = testEnvironment();
-expect;
-log;
