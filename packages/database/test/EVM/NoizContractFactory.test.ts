fb804e834790400d0ddfa7d32455fa961213f7fb
ariannatnl <arianna.minucci@me.com>
2022-12-16 16:47:17 +0100

    File test Contract Factory

--- packages/database/test/EVM/NoizContractFactory.test.ts
+++ packages/database/test/EVM/NoizContractFactory.test.ts
@@ -2,10 +2,19 @@
 import { testEnvironment } from "@zionstate/test";
 // rappresenta il codice che stiamo testando.
 // deve essere preso dall'endpoint che stiamo valutando
-import { NoizContractFactory } from "../../src/EVM";
+import { NoizContractFactory } from "../../EVM";
 NoizContractFactory;
 
 const { expect, log } = testEnvironment();
 expect;
 log;
-log("prova");
+
+// la prima funzione ci permette di creare l'intestazione dell nostro test
+describe("NoizContractFactory_v1", () => {
+  // questa chiamata è quella che crea il print con validazione `✔`
+  it("dovrebbe stampare nella console: NoizContractFactory_v1", () => {
+    expect(NoizContractFactory.name).to.be.equal(
+      "NoizContractFactory_v1"
+    );
+  });
+});
