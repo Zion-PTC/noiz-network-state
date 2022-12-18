bcfad2fdc172e666f96f5b2514eb51de2c289197
Giacomo Gagliano <giacomo.gagliano@me.com>
2022-12-18 16:29:19 +0100

    styled a little
    +

--- packages/ui/src/pages/index.tsx
+++ packages/ui/src/pages/index.tsx
@@ -1,4 +1,4 @@
-import React, { Component, lazy } from "react";
+import React, { Component } from "react";
 import styled from "styled-components";
 
 interface FieldProps {
@@ -63,19 +63,26 @@ class MetamaskWallet extends Component<
     if (eth?.isBraveWallet) this.setIsBraveWallet(true);
   }
 
+  negative = "#a9787d";
+  positive = "#16a200";
+
   Field = (props: FieldProps) => (
     <div className={props.className}>
-      <p>{props.field}</p>
+      <p id="field">{props.field}</p>
       <p id="is-eth-res">{props.value.toString()}</p>
     </div>
   );
   StyledField = styled(this.Field)`
     display: inline-flex;
     justify-content: space-between;
+    #field {
+      color: ${props => (props.value ? "black" : "grey")};
+    }
     #is-eth-res  {
       padding-left: 0.3rem;
       font-weight: bold;
-      color: ${props => (props.value ? "green" : "red")};
+      color: ${props =>
+        props.value ? this.positive : this.negative};
     }
   `;
 
@@ -105,7 +112,7 @@ class MetamaskWallet extends Component<
     return (
       <div className={className}>
         <div id="container">
-          <p>Wallet test page</p>
+          <h3>Wallet test page</h3>
           <Field {...isEthereum} />
           <Field {...isMetamask} />
           <Field {...isTrust} />
@@ -118,19 +125,24 @@ class MetamaskWallet extends Component<
   StyledLayout = styled(this.Layout)`
     display: grid;
     place-items: center;
-    background: rgb(7, 50, 56);
-    background: linear-gradient(
-      90deg,
-      hsl(187, 77%, 12%) 35%,
-      hsl(190, 77%, 28%) 100%
-    );
     #container {
+      box-shadow: 0px 0px 11px -2px rgba(0, 0, 0, 0.4);
       background-color: hsl(203, 78%, 77%);
-      border-radius: 0.5rem;
+      border-radius: 1rem;
+      border: 0.3rem solid
+        hsl(
+          203.1372549019608,
+          96.22641509433963%,
+          31.176470588235293%
+        );
       display: flex;
       width: max-content;
       flex-direction: column;
       padding: 2rem;
+      h3 {
+        text-align: center;
+        margin-bottom: 0.3rem;
+      }
     }
   `;
 
@@ -142,6 +154,23 @@ class MetamaskWallet extends Component<
   }
 }
 
+const sideColor = "#185f6d";
+
+const Div = styled.div`
+  display: grid;
+  background: rgb(7, 50, 56);
+  background: linear-gradient(
+    90deg,
+    ${sideColor} 0%,
+    hsl(190, 77%, 28%) 50%,
+    ${sideColor} 100%
+  );
+`;
+
 export default function index() {
-  return <MetamaskWallet />;
+  return (
+    <Div>
+      <MetamaskWallet />
+    </Div>
+  );
 }
