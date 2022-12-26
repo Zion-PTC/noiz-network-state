0bcf9ae41a4cb5a5037b2542dd7891d57ce05579
Giacomo Gagliano <giacomo.gagliano@me.com>
2022-12-26 22:33:40 +0100

    create page

--- packages/ui/src/pages/classes/md/chart-in-md/index.tsx
+++ packages/ui/src/pages/classes/md/chart-in-md/index.tsx
@@ -1,7 +1,32 @@
 import React from "react";
+import "../../../../HTML";
+import { Md } from "../../../../HTML/React/classes";
+import { readAndParse } from "../../../../library";
 
-export default function index() {
+interface Datas {
+  data: string;
+}
+
+export function getStaticProps() {
+  const postPath = "posts";
+  const file = "test-chart";
+  const result = readAndParse(postPath, file);
+
+  console.log(result);
+  return {
+    props: {
+      data: result,
+    },
+  };
+}
+
+export default function index({ data }: Datas) {
   return (
-    <div>index</div>
-  )
+    <Md
+      contentString={data}
+      md_raw_react
+      layout="main"
+      style="defaultStyle"
+    ></Md>
+  );
 }
