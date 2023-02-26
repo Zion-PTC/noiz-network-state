5d7f4250b0f46a107d14d838b7532429ce6f097a
Giacomo Gagliano <giacomo.gagliano@me.com>
2023-02-26 23:10:59 +0100

    initial commit

---
+++ apps/spesa/src/pages/api/spesa/Spesa/lib/generateRandom.ts
@@ -0,0 +1,8 @@
+import { js } from "@zionstate/zionbase/utils";
+export let createRandom = js.lib.createRandom;
+
+export let generateRandom = () =>
+  Math.round(Math.random() * 100_000_000_000).toString(16);
+
+// @ts-expect-error
+window.createra = createRandom;
